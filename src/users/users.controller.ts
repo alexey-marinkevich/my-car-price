b02dbaccc0002f4b81cf457ce179db8ded81a3bd
Alex Marinkevych <24780101+alexey-marinkevich@users.noreply.github.com>
2022-12-05 14:22:26 +0100

    Add findUser to the users.controllers

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,13 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  Get,
+  Patch,
+  Delete,
+  Param,
+  Query,
+} from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
 
@@ -10,5 +19,9 @@ export class UsersController {
   createUser(@Body() body: CreateUserDto) {
     const { email, password } = body;
     this.usersService.create(email, password);
+  }
+  @Get('/:id')
+  findUser(@Param('id') id: string) {
+    return this.usersService.findOne(parseInt(id));
   }
 }
