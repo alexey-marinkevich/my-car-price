2f1f3041197d678cff16f84f28baf83ff8b63c21
Alex Marinkevych <24780101+alexey-marinkevich@users.noreply.github.com>
2022-12-05 12:37:52 +0100

    Add signup route for users.controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { UsersService } from './users.service';
 
-@Controller('users')
-export class UsersController {}
+@Controller('auth')
+export class UsersController {
+  constructor(private usersService: UsersService) {}
+
+  @Post('/signup')
+  createUser(@Body() body: CreateUserDto) {
+    const { email, password } = body;
+    this.usersService.create(email, password);
+  }
+}
