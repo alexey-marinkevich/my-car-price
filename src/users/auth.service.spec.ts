94b55121d867130bc8fba2c5eb4604779b024065
Alex Marinkevych <24780101+alexey-marinkevich@users.noreply.github.com>
2022-12-07 14:51:15 +0100

    Add unit tests to auth.service.spec

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -45,4 +45,55 @@ describe('AuthService', () => {
   it('can create instance of an auth service', async () => {
     expect(service).toBeDefined();
   });
+  it('creates a new user with hashed password', async () => {
+    const requestBody = {
+      email: 'test@test.com',
+      password: 'test',
+    };
+
+    const user = await service.signUp(requestBody);
+    expect(user.password).not.toEqual(requestBody.password);
+  });
+
+  it('throws an error if user signs up with email that is in use', async () => {
+    const requestBody = {
+      email: 'a',
+      password: '1',
+    };
+
+    await service.signUp(requestBody);
+
+    await expect(service.signUp(requestBody)).rejects.toThrow(
+      BadRequestException,
+    );
+  });
+
+  it('throws if sign in is called with an unused email', async () => {
+    const requestBody = {
+      email: 'test@test.com',
+      password: 'test',
+    };
+
+    await expect(service.signIn(requestBody)).rejects.toThrow(
+      BadRequestException,
+    );
+  });
+
+  it('throws if an invalid password is provided', async () => {
+    const user = {
+      email: 'test@test.com',
+      password: 'test',
+    };
+
+    const requestBody = {
+      email: 'test@test.com',
+      password: 'testtest',
+    };
+
+    await service.signUp(user);
+
+    await expect(service.signIn(requestBody)).rejects.toThrow(
+      BadRequestException,
+    );
+  });
 });
