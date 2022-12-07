02a6a305b2bf98e89f6b1ecbe57f2f9416c53b31
Alex Marinkevych <24780101+alexey-marinkevich@users.noreply.github.com>
2022-12-07 14:49:41 +0100

    Create fake testing service

---
+++ src/users/auth.service.spec.ts
@@ -0,0 +1,48 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { BadRequestException, NotFoundException } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { User } from './user.entity';
+import { UsersService } from './users.service';
+
+describe('AuthService', () => {
+  let service: AuthService;
+  let fakeUsersService: Partial<UsersService>;
+
+  beforeEach(async () => {
+    const users: User[] = [];
+
+    fakeUsersService = {
+      findByEmail: (email: string) => {
+        const filteredUsers = users.filter((user) => user.email === email);
+        return Promise.resolve(filteredUsers);
+      },
+      create: (email: string, password: string) => {
+        const user = {
+          id: Math.floor(Math.random() * 99999),
+          email,
+          password,
+        } as User;
+
+        users.push(user);
+
+        return Promise.resolve(user);
+      },
+    };
+
+    const module = await Test.createTestingModule({
+      providers: [
+        AuthService,
+        {
+          provide: UsersService,
+          useValue: fakeUsersService,
+        },
+      ],
+    }).compile();
+
+    service = module.get(AuthService);
+  });
+
+  it('can create instance of an auth service', async () => {
+    expect(service).toBeDefined();
+  });
+});
