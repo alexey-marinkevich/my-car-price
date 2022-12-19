77f5550013be7b47499fe33dc12935901dc18bb9
Alex Marinkevych <24780101+alexey-marinkevich@users.noreply.github.com>
2022-12-19 18:39:02 +0100

    Update reports estimation query

--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -37,14 +37,12 @@ export class ReportsService {
       .select('AVG(price)', 'price')
       .where('make = :make', { make })
       .andWhere('model = :model', { model })
-      .andWhere('model = :model', { model })
       .andWhere('lng - :lng BETWEEN -5 AND 5', { lng })
       .andWhere('lat - :lat BETWEEN -5 AND 5', { lat })
       .andWhere('year - :year BETWEEN -3 AND 3', { year })
       .andWhere('approved IS TRUE')
       .orderBy('ABS(mileage - :mileage)', 'DESC')
       .setParameters({ mileage })
-      .limit(5)
       .getRawOne();
   }
 }
