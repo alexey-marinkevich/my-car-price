9a2e439c07b1f6e9a6509bdd6a73eda31c859fd9
Alex Marinkevych <24780101+alexey-marinkevich@users.noreply.github.com>
2022-12-19 17:12:39 +0100

    Cleanup

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,11 @@ import { Module, ValidationPipe, MiddlewareConsumer } from '@nestjs/common';
 import { APP_PIPE } from '@nestjs/core';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
 import { User } from './users/user.entity';
 import { Report } from './reports/report.entity';
+
 const cookieSession = require('cookie-session');
 
 @Module({
@@ -30,9 +29,8 @@ const cookieSession = require('cookie-session');
     UsersModule,
     ReportsModule,
   ],
-  controllers: [AppController],
+  controllers: [],
   providers: [
-    AppService,
     {
       provide: APP_PIPE,
       useValue: new ValidationPipe({
